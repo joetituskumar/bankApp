d2a5ad3bba5962bda5630df9488c8cfc2fb80fd3
Joel Titus <joeltituskumar@gmail.com>
2021-07-15 23:09:50 +0530

    final commit

---
+++ app/src/main/java/com/example/sample1/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.sample1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+                              startActivity(Intent(this,LoginActivity::class.java))
+        },2000)
+    }
+}
